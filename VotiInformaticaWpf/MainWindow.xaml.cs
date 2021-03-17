4ed7857d1db4fd496e85836af72b9ca92c9d21bc
tommaso lauria <tommaso.lauria9@gmail.com>
2021-03-17 12:38:23 +0100

    aggiustamento try

--- VotiInformaticaWpf/MainWindow.xaml.cs
+++ VotiInformaticaWpf/MainWindow.xaml.cs
@@ -31,37 +31,39 @@ namespace VotiInformaticaWpf
 
             if (txtVoto1.Text != "" && txtVoto2.Text != "" && txtVoto3.Text != "" && txtQuartoVoto.Text != "")
             {
-                double voto1 = double.Parse(txtVoto1.Text);
-                double voto2 = double.Parse(txtVoto2.Text);
-                double voto3 = double.Parse(txtVoto3.Text);
-                double voto4 = double.Parse(txtQuartoVoto.Text);
-                if (voto1 > 10 || voto1 < 2)
+                try
                 {
-                    MessageBox.Show("attenzione inserire un voto da 2 a 10", "attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
-                    txtVoto1.Clear();
-                    txtVoto1.Focus();
-                }
-                else if (voto2 > 10 || voto2 < 2)
-                {
-                    MessageBox.Show("attenzione inserire un voto da 2 a 10", "attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
-                    txtVoto2.Clear();
-                    txtVoto2.Focus();
-                }
-                else if (voto3 > 10 || voto3 < 2)
-                {
-                    MessageBox.Show("attenzione inserire un voto da 2 a 10", "attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
-                    txtVoto3.Clear();
-                    txtVoto3.Focus();
-                }
-                else if (voto4 > 10 || voto4 < 2)
-                {
-                    MessageBox.Show("attenzione inserire un voto da 2 a 10", "attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
-                    txtQuartoVoto.Clear();
-                    txtQuartoVoto.Focus();
-                }
-                else
-                {
-                    try
+
+
+                    double voto1 = double.Parse(txtVoto1.Text);
+                    double voto2 = double.Parse(txtVoto2.Text);
+                    double voto3 = double.Parse(txtVoto3.Text);
+                    double voto4 = double.Parse(txtQuartoVoto.Text);
+                    if (voto1 > 10 || voto1 < 2)
+                    {
+                        MessageBox.Show("attenzione inserire un voto da 2 a 10", "attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        txtVoto1.Clear();
+                        txtVoto1.Focus();
+                    }
+                    else if (voto2 > 10 || voto2 < 2)
+                    {
+                        MessageBox.Show("attenzione inserire un voto da 2 a 10", "attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        txtVoto2.Clear();
+                        txtVoto2.Focus();
+                    }
+                    else if (voto3 > 10 || voto3 < 2)
+                    {
+                        MessageBox.Show("attenzione inserire un voto da 2 a 10", "attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        txtVoto3.Clear();
+                        txtVoto3.Focus();
+                    }
+                    else if (voto4 > 10 || voto4 < 2)
+                    {
+                        MessageBox.Show("attenzione inserire un voto da 2 a 10", "attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        txtQuartoVoto.Clear();
+                        txtQuartoVoto.Focus();
+                    }
+                    else
                     {
 
 
@@ -75,12 +77,12 @@ namespace VotiInformaticaWpf
                         else
                             lblPromossoBocciato.Content = "Bocciato";
                     }
-                    catch (Exception ex)
-                    {
-                        txt_RisultatoMedia.Text = ex.Message;
-                    }
                 }
-                
+                catch (Exception ex)
+                {
+                    txt_RisultatoMedia.Text = ex.Message;
+                }
+
             }
             else
             {
